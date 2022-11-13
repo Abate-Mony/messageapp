09eaa0a7504edd23863fd2b64cb7b565f0ba068d
mmanuel <bateemma14@gmail.com>
2022-11-13 21:31:02 +0100

    trying

--- index.js
+++ index.js
@@ -52,15 +52,14 @@ require("express-async-errors")
 const path = require("path")
 const app = express()
 const cors = require("cors")
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
-    res.setHeader('Access-Control-Allow-Headers', "*");
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-});
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
 
-// app.use(cors())
-app.use(cors({ origin: true }));
+app.use(cors(corsOptions))
+
 
 const port = process.env.PORT || process.env.port
 app.use(express.json())
