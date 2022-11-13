9339dbd6055ceb31c540b666b34d4967c2b93f7a
mmanuel <bateemma14@gmail.com>
2022-11-13 22:52:52 +0100

    it must work3

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ app.use(cors())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
     next()
-
 })
 
 
